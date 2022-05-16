520256d6014918b87f787fe4a0400c70bfa57e0a
Илья <fetisov.ilya.andreevich@gmail.com>
2022-05-16 22:31:48 +0300

    выполнил 1 задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,6 +2,13 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
-
+        byte a = 10;
+        short b = 10;
+        int c = 10;
+        long d = 10L;
+        float e = 10.5f;
+        double f = 10;
+        char g = 1;
+        boolean x = false;
     }
 }
