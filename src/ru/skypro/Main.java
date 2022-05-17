5c131b41349a33a8d47fd7ae0dee4b76a234ffbe
Илья <fetisov.ilya.andreevich@gmail.com>
2022-05-17 14:08:45 +0300

    выполнил 2 задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,13 +2,21 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
-        byte a = 10;
-        short b = 10;
-        int c = 10;
-        long d = 10L;
-        float e = 10.5f;
-        double f = 10;
-        char g = 1;
-        boolean x = false;
+        //         Задание 1
+//        byte a = 10;
+//        short b = 10;
+//        int c = 10;
+//        long d = 10L;
+//        float e = 10.5f;
+//        double f = 10;
+//        char g = 1;
+//        boolean x = false;
+
+//        задание 2
+
+        float weightBoxer_1 = 78.2f;
+        float weightBoxer_2 = 82.7f;
+        System.out.println("Общий вес боксеров " + (weightBoxer_1 + weightBoxer_2) + " кг.");
+        System.out.println("Разница в весе боксеров " + (weightBoxer_2 - weightBoxer_1) + " кг.");
     }
 }
