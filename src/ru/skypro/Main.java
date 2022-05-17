c84a531702fcf52d9023ff3b509b3c14d73e03d4
Илья <fetisov.ilya.andreevich@gmail.com>
2022-05-17 14:55:37 +0300

    выполнил 3 задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -13,10 +13,20 @@ public class Main {
 //        boolean x = false;
 
 //        задание 2
+//
+//        float weightBoxer_1 = 78.2f;
+//        float weightBoxer_2 = 82.7f;
+//        System.out.println("Общий вес боксеров " + (weightBoxer_1 + weightBoxer_2) + " кг.");
+//        System.out.println("Разница в весе боксеров " + (weightBoxer_2 - weightBoxer_1) + " кг.");
 
-        float weightBoxer_1 = 78.2f;
-        float weightBoxer_2 = 82.7f;
-        System.out.println("Общий вес боксеров " + (weightBoxer_1 + weightBoxer_2) + " кг.");
-        System.out.println("Разница в весе боксеров " + (weightBoxer_2 - weightBoxer_1) + " кг.");
+//        задание 3
+
+        int bananaWeight = 80;
+        int milkWeight = 105;
+        int ice_creamWeight = 100;
+        int eggWeight = 70;
+        float cocktailWeight = bananaWeight * 5 + milkWeight * 2 + ice_creamWeight * 2 + eggWeight * 4;
+        System.out.println("Вес коктейля " + cocktailWeight + " грамм, или " + cocktailWeight/1000 + " кг.");
+
     }
 }
